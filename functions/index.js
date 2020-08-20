80289be56c82fd1216ae4bfe6a01c0d329d079a8
Kaoru Ishizawa <36197417+10kaoru12@users.noreply.github.com>
2020-08-20 23:58:57 +0900

    write readme.

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,22 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
+const request = require('request');
+const crypto = require('crypto');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.TradingFunction = functions
 .https
 .onRequest((request, response) => {
-  let marketsResponse = axios.get("https://api.bitflyer.com/v1/markets")
+  axios.get("https://api.bitflyer.com/v1/markets")
   .then(res=>{
-      response.send(res.data);
-      return;
+    response.send(res.data);
+    return;
   })
   .catch(err=>{
-      response.send(err);
+    response.send(err);
+    return;
   });
 });
+
+function (){
+
+}
