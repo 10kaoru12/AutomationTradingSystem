b8eb74a791eed5bf15c0553959c637cc3a880baa
Kaoru Ishizawa <36197417+10kaoru12@users.noreply.github.com>
2020-08-21 01:51:10 +0900

    認証ロジックの追加

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,13 @@ const crypto = require('crypto');
 exports.TradingFunction = functions
 .https
 .onRequest((request, response) => {
+
+  //post request以外の通信の遮断
+  if(request.method !== "POST"){
+    response.status(405).send('Access Not Allowed');
+    return;
+  }
+
   axios.get("https://api.bitflyer.com/v1/markets")
   .then(res=>{
     response.send(res.data);
@@ -15,8 +22,19 @@ exports.TradingFunction = functions
     response.send(err);
     return;
   });
+
+  //認証処理
+  //firebase環境変数からapikeyとapisecretkeyを取得
+  // const key=functions.config().api.key;
+  // const secret=functions.config().api.secret;
+  const timeStanp=Date.now().toString();
+  const method="POST";
+  const path="https://api.bitflyer.com/v1/me/sendchildorder";
+  const body=JSON.stringify({
+    product_code:"BTC_JPY",
+    child_order_type:"LIMIT",
+    side:"BUY",
+    price:30000,
+    size:0.1
+  });
 });
-
-function (){
-
-}
